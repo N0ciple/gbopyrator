c4193bd7edb29db9d8ceb357772b09dd3e6ba567
Robin Dupont <robin.dpnt@gmail.com>
2023-05-06 13:39:13 +0200

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_requirements(file):
 
 setup(
     name="gbopyrator",
-    version="0.3",
+    version="0.4",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
