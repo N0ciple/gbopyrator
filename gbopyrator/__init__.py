81a56753a208f3bf32eb2069a6171dd7cd7049b2
Robin Dupont <robin.dpnt@gmail.com>
2023-05-06 13:34:33 +0200

    bump version

--- gbopyrator/__init__.py
+++ gbopyrator/__init__.py
@@ -2,7 +2,7 @@ from .cartridge_utils import CartridgeReader
 from pkg_resources import resource_filename
 import json
 
-__version__ = "0.2"
+__version__ = "0.3"
 
 def load_roms_db(filename=resource_filename(__name__, "gb_gbc_roms_info.json")):
     with open(filename, "r") as file:
